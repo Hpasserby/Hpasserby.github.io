c19ade312b311190b4e3c5456527536f2aa4cd99
Hpasserby <hgy5945@163.com>
2019-02-25 17:54:49 +0800

    Site updated: 2019-02-25 17:54:35

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Sat Dec 08 2018 01:44:21 GMT+0800 (中国标准时间)
+// build time:Mon Feb 25 2019 17:50:43 GMT+0800 (GMT+08:00)
 !function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-globe"></i><span>copy</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
 //rebuild by neat 
