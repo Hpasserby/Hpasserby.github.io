a8d5536dcb7e4a03b16b15433ec2648d3754a3e3
Hpasserby <hgy5945@163.com>
2019-03-14 20:19:48 +0800

    Site updated: 2019-03-14 20:19:42

--- js/transition.js
+++ js/transition.js
@@ -1,3 +1,3 @@
-// build time:Thu Mar 14 2019 19:15:53 GMT+0800 (中国标准时间)
+// build time:Thu Mar 14 2019 20:19:32 GMT+0800 (中国标准时间)
 $(function(){$(".layout").velocity("stop").velocity("transition.slideUpIn",{delay:500,duration:1e3,easing:"easeInOutQuart",complete:function(){if($(".sidebar-toc").length>0){setTimeout(function(){$("#toggle-sidebar").click()},200)}}});$("#top-container").velocity("stop").velocity("transition.fadeIn",{delay:500,duration:1e3,easing:"easeInOutQuart"})});
 //rebuild by neat 
