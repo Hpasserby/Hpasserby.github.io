3d3e0c6dc285d11f9c44174ec08f2d5ef23c2cfb
Hpasserby <hgy5945@163.com>
2019-10-30 22:30:30 +0800

    Site updated: 2019-10-30 22:30:16

--- js/hexo-theme-melody.js
+++ js/hexo-theme-melody.js
@@ -1,3 +1,3 @@
-// build time:Thu Oct 17 2019 11:46:22 GMT+0800 (中国标准时间)
+// build time:Wed Oct 30 2019 22:30:14 GMT+0800 (中国标准时间)
 
 //rebuild by neat 
--- js/transition.js
+++ js/transition.js
@@ -1,3 +1,3 @@
-// build time:Thu Oct 17 2019 11:46:22 GMT+0800 (中国标准时间)
+// build time:Wed Oct 30 2019 22:30:15 GMT+0800 (中国标准时间)
 $(function(){$(".layout").velocity("stop").velocity("transition.slideUpIn",{delay:500,duration:1e3,easing:"easeInOutQuart",complete:function(){if($(".sidebar-toc").length>0){setTimeout(function(){$("#toggle-sidebar").click()},200)}}});$("#top-container").velocity("stop").velocity("transition.fadeIn",{delay:500,duration:1e3,easing:"easeInOutQuart"})});
 //rebuild by neat 
